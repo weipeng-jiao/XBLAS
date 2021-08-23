a09c574c95fdca0557ec021401d5ce00aaa0f8c2
weipeng <weipeng.x.jiao@gmail.com>
2021-08-23 21:42:10 +0800

    xxdot

---
+++ XBLAS/ddot.cpp
@@ -0,0 +1,39 @@
+
+#include "xblas.h"
+#include "ehm.h"
+#include <cmath>
+
+
+double ddot (const  int n,  const double *x, const int incx, const double *y, const int incy)
+{
+    double sum=0;
+    int xstep=0;
+    int ystep=0;
+    int xnums=sizeof(x)/sizeof(x[0]);    //number of x-vector element
+    int ynums=sizeof(y)/sizeof(y[0]);    //number of y-vector element
+    int ixnums=1+(n-1)*incx; //number of interval
+    int iynums=1+(n-1)*incy; //number of interval
+   
+    //exception handling
+    BlasErrorIF(n<=0||incx<=0||xnums<ixnums||ynums<iynums||xnums!=ynums);
+
+ 
+    //calculation
+    if(incx==1&&incy==1)
+    {
+        for(int i=0;i<n;i++)
+        {
+            sum+=x[i]*y[i];
+        }
+    }
+    else
+    {
+       for(int i=0;i<n;i++)
+        {
+            sum+=x[xstep]*y[ystep];
+            xstep+=incx;
+            ystep+=incy;
+        } 
+    }
+    return sum;
+}
